8b5d6437df35813b3175ffcdf65ae7ba9a34b502
CollinStubbs <collin.stubbs@uoit.net>
2014-09-27 23:21:36 -0400

    Did some stuff to homepage

--- src/collin/wearhacks/networknow/Homepage.java
+++ src/collin/wearhacks/networknow/Homepage.java
@@ -34,10 +34,9 @@ import android.content.Intent;
  
 public class Homepage extends Activity {
  
-    EditText etResponse;
-    TextView tvIsConnected;
+    
     int spInt1, spInt2, spInt3;
-    String PUUID;
+    
     ArrayList<String> arrays = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +46,7 @@ public class Homepage extends Activity {
         
  
         EditText editText = (EditText)findViewById(R.id.notaspinner);
-        //editText.setText("UserName", TextView.BufferType.EDITABLE);	
+        	
         editText.setTextColor(0xAA000000);
         
         Button button = (Button) findViewById(R.id.submit);
@@ -167,73 +166,10 @@ public class Homepage extends Activity {
         });
         
         
-        // get reference to the views
-       /* etResponse = (EditText) findViewById(R.id.etResponse);
-        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
- 
-        // check if you are connected or not
-        if(isConnected()){
-            tvIsConnected.setBackgroundColor(0xFF00CC00);
-            tvIsConnected.setText("You are conncted");
-        }
-        else{
-            tvIsConnected.setText("You are NOT conncted");
-        }
- 
-        // show response on the EditText etResponse 
-        etResponse.setText(GET("http://hmkcode.com/examples/index.php"));*/
+      
  
     }
    
 
- 
-    public static String GET(String url){
-        InputStream inputStream = null;
-        String result = "";
-        try {
- 
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
- 
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
- 
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
- 
-            // convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
- 
-        } catch (Exception e) {
-            Log.d("InputStream", ""+ e.getLocalizedMessage());
-        }
- 
-        return result;
-    }
- 
-    // convert inputstream to String
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
- 
-    }
- 
-    // check network connection
-    public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) 
-                return true;
-            else
-                return false;   
-    }
+
 }
