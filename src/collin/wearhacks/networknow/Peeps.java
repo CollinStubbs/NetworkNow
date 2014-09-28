e17aaaaf4b5bd95964f1402bdee61be70ba506a1
CollinStubbs <collin.stubbs@uoit.net>
2014-09-28 06:32:54 -0400

    push before a nap

--- src/collin/wearhacks/networknow/Peeps.java
+++ src/collin/wearhacks/networknow/Peeps.java
@@ -1,16 +1,61 @@
 package collin.wearhacks.networknow;
 
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Peeps extends ActionBarActivity {
-
+	String temp[];
+	ListView list;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_peeps);
+		ArrayList<String> peeps = new ArrayList<String>();
+		HTTPRequests c = new HTTPRequests();
+		String userMatches = "http://104.131.105.6:3000/matches/weeks/hotspot/b9407f30-f5f8-466e-aff9-25556b57fe6d5555522222";
+		try {
+			temp = c.JSON2Array(c.getJSON(userMatches));
+			for(int i = 0; i<temp.length; i++){
+				peeps.add(temp[i]);
+			}
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	              android.R.layout.simple_list_item_1, android.R.id.text1, temp);
+		 list = (ListView) findViewById(R.id.list);
+		 list.setAdapter(adapter);
+		  list.setOnItemClickListener(new OnItemClickListener() {
+			  
+              @Override
+              public void onItemClick(AdapterView<?> parent, View view,
+                 int position, long id) {
+                
+               // ListView Clicked item index
+               int itemPosition     = position;
+               
+               // ListView Clicked item value
+               String  itemValue    = (String) listView.getItemAtPosition(position);
+                  
+                // Show Alert 
+                Toast.makeText(getApplicationContext(),
+                  "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                  .show();
+             
+              }
+
+         }); 
+		
 	}
 
 	@Override
